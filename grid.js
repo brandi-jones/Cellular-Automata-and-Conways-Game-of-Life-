535716fab5386b47c50947a13115dc7c1248e026
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-06-25 13:59:20 -0400

    logic is down for rules for life/death of cells and drawing

--- grid.js
+++ grid.js
@@ -53,7 +53,64 @@ function draw() {
         
         }
     }
+    
     noLoop()
+
+    //create a new 2Darray for the "next" array after a cycle has completed
+    let nextGrid = new Array(columns);
+    //loop over columns to create new arrays at each column
+    for (var i = 0; i < columns; i++) { 
+        nextGrid[i] = new Array(rows);
+    }
+
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
+
+            let cell = grid[i][j];
+            let neighbors = countNeighbors(grid, i, j);
+
+            //if cell is alive and neighbors are less than 2 (dies of underpopulation)
+            if (cell == 1 && neighbors < 2) {
+                nextGrid[i][j] = 0;
+            }
+            //if cell is alive and has exactly 2 or 3 neighbors (lives on to the next generation)
+            else if (cell = 1  && neighbors == 2 || neighbors == 3){
+                nextGrid[i][j] = cell;
+            }
+            //if cell is alive and has more than 3 neighbors (dies of overpopulation)
+            else if (cell == 1 && neighbors > 3) {
+                nextGrid[i][j] = 0;
+            }
+            //if cell is dead and has exactly 3 neighbors (born from reproduction)
+            else if (cell == 0 && neighbors == 3) {
+                nextGrid[i][j] = 1;
+            }
+            //any other cells die in the next generation
+            else {
+                nextGrid[i][j] = 0;
+            }
+
+        }
+    }
+
+    grid = nextGrid;
+}
+
+function countNeighbors(grid, x, y) {
+    let count = 0;
+
+    //loop over all of a cell's neighbors, and if it is a live cell (value of 1), the count will be increased by 1. Otherwise, 0 will be added to count (dead cell)
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+          let column = (x + i + columns) % columns;
+          let row = (y + j + rows) % rows;
+          count += grid[column][row];
+        }
+    }
+    //subtract itself
+    count -= grid[x][y];
+
+    return count;
 }
 
 
