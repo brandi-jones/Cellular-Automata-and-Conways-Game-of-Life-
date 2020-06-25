aa96b8ee9fd0e039a87e10f1acaee175e8207f4f
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-06-24 22:20:34 -0400

    grid is now showing up properly

--- grid.js
+++ grid.js
@@ -1,21 +1,61 @@
+let grid
+let columns;
+let rows;
+let resolution = 50;
 
 //create a grid full of 0's of the specific size of columns/rows
-function setupGrid(columns, rows) {
+function setup() {
+
+    //create p5.js canvas for sketching later
+    createCanvas(500, 500);
+
+    columns = width / resolution;
+    rows = height / resolution;
+
     //create inital array
-    let grid = new Array(columns);
+    grid = new Array(columns);
     
     //loop over columns to create new arrays at each column
-    for (var x = 0; x < columns; x++) { 
-        grid[x] = new Array(rows);
+    for (var i = 0; i < columns; i++) { 
+        grid[i] = new Array(rows);
 
         //loop over each specific column's array and fill in 0's according to the specific # of rows
-        for (var y = 0; y < rows; y++) { 
-            grid[x][y] = 0;
+        for (var j = 0; j < rows; j++) { 
+            grid[i][j] = 0;
         }
     }
 
-    return grid;
 }
 
-let grid = setupGrid(25, 25);
+//sketch canvas with p5.js functions
+function draw() {
+    background(66, 78, 245);
+
+    for (var i = 0; i < columns; i++){
+        
+        for (var j = 0; j < rows; j++) {
+
+            let x = i * resolution;
+            let y = j * resolution;
+    
+            //if alive
+            if (grid[i][j] == 1) {
+                fill(255);
+            }
+            //if dead
+            else {
+                fill(66, 78, 245);
+            }
+
+            //draw a rectangle at position x,y on the canvas with width and height
+            rect(x, y, 50, 50) 
+            console.log("hit this");
+        
+        }
+    }
+    noLoop()
+}
+
+
+
 
